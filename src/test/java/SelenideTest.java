b1c33c60546d8aae45b2467ce60d1aa567db0485
Татьяна Смыслова <l.dalecrua@gmail.com>
2022-08-11 13:44:19 +0300

    внесла изменения в тест

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,6 +1,10 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
@@ -8,15 +12,23 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(3);
+
     @Test
     void test1(){
         open("http://localhost:9999");
-        $("[data-test-id=city] .input__control").setValue("Майкоп");
-        $("[data-test-id=date] [placeholder=\"Дата встречи\"]").setValue("13.08.2022");
-        $("[data-test-id=name] [name=name]").setValue("Мамин-Сибиряк Дмитрий");
-        $("[data-test-id=phone] [name=phone]").setValue("+79649971830");
-        $("[data-test-id=agreement]>.checkbox__box").click();
+        $("[data-test-id='city'] .input__control").setValue("Майкоп");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] [name=name]").setValue("Мамин-Сибиряк Дмитрий");
+        $("[data-test-id='phone'] [name=phone]").setValue("+79649971830");
+        $("[data-test-id='agreement']>.checkbox__box").click();
         $("button>.button__content").click();
-        $(withText("Успешно!")).should(visible, Duration.ofSeconds(20));
+        $(withText("Успешно!")).should(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 }
